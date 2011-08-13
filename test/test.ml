eed4a552a2fa8b1b18455b341ac8115d163b2fa2
Niki Yoshiuchi <aplusbi@gmail.com>
2011-08-13 14:29:11 -0400

    Fixed test code and switched it to bytecode

--- test/test.ml
+++ test/test.ml
@@ -21,6 +21,11 @@ let write_midi st event =
     let {Smf.midi_buffer=buffer} = event in
     let size = (Bigarray.Array1.dim buffer) in
     let msg = match size with
+    | 1 ->
+            let a = (Bigarray.Array1.get buffer 0) in
+            let str = (string_of_int a) ^ "\n" in
+            ignore (Unix.write Unix.stdout str 0 (String.length str));
+            Portmidi.message a 0 0
     | 2 ->
             let a, b = (Bigarray.Array1.get buffer 0), (Bigarray.Array1.get buffer 1) in
             let str = (string_of_int a) ^ " " ^ (string_of_int b) ^ "\n" in
@@ -35,23 +40,33 @@ let write_midi st event =
     in
     Portmidi.write_short st Int32.zero msg
 
+let wait_loop secs =
+    let rec wait elapsed =
+        if elapsed < secs then
+            let t_ms = (Portmidi.Time.time ()) in
+            let t = (Int32.to_float t_ms) /. 1000. in
+            Portmidi.Time.sleep 1; wait t
+        else
+            ()
+    in
+    wait 0.
+
 let _ =
     Portmidi.init ();
-    Portmidi.Time.start 1;
     let d = get_device Output in
     let st = Portmidi.open_output d 8 0 in
     let smf = Smf.load "test/bach_gavotte.mid" in
+    Portmidi.Time.start 1;
     while true do
         let event = Smf.get_next_event smf in
         match Smf.event_is_metadata event with
         | true -> ()
         | false ->
                 begin
-                    let {Smf.time_pulses=wait} = event in
-                    let wait = wait / 100 in
-                    let str = (string_of_int wait) ^ "\n" in
+                    let {Smf.time_seconds=wait} = event in
+                    let str = (string_of_float wait) ^ "\n" in
                     ignore (Unix.write Unix.stdout str 0 (String.length str));
-                    (*Portmidi.Time.sleep wait;*)
+                    wait_loop wait;
                     write_midi st event;
                     ()
                 end;
